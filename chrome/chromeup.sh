facf7b7a18cd5ed0ec7a42a7a7e97f64aefe24f8
nu11secur1ty <systadmi@abv.bg>
2017-11-18 10:59:06 -0500

    Update chromeup.sh

--- chrome/chromeup.sh
+++ chrome/chromeup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
-sed -i '$d' /usr/bin/google-chrome-stable
 echo -e 'exec -a "$0" "$HERE/chrome" "$@" --no-sandbox' >> /usr/bin/google-chrome-stable
+sleep 3;
+sed -i '$d' /usr/bin/google-chrome-stable
 exit;
