1f942748c240217533463a862aeb5de99f1fc220
nu11secur1ty <systadmi@abv.bg>
2018-01-06 12:41:49 +0200

    Update chrome.sh

--- chrome/chrome.sh
+++ chrome/chrome.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+chmod a+x /usr/bin/google-chrome
+chmod a+x /usr/bin/google-chrome-stable
 sed -i '$d' /usr/bin/google-chrome-stable
 echo -e 'exec -a "$0" "$HERE/chrome" "$@" --user-data-dir --no-sandbox' >> /usr/bin/google-chrome-stable
 exit;
