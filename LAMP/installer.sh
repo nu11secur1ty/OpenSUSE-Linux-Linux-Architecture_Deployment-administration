1eae9d5236cb2a04634f4e82f41255f7b45f8136
nu11secur1ty <venvaropt@gmail.com>
2019-07-23 13:02:31 +0300

    Update installer.sh

--- LAMP/installer.sh
+++ LAMP/installer.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
 # install Apache2
- zypper in -n apache2
+ zypper in apache2 -y
  systemctl start apache2
  systemctl enable apache2
 
 # Install PHP5
- zypper in -n php5 php5-mysql apache2-mod_php5
+ zypper in php5 php5-mysql apache2-mod_php5 -y
  a2enmod php5
 
 # Install MAriadDB
- zypper in -n mariadb mariadb-tools
+ zypper in -n mariadb mariadb-tools -y
  systemctl start mysql
  systemctl enable mysql
  
