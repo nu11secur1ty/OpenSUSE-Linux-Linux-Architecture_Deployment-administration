214f10d066055499857f0bc4656518775c3e242c
nu11secur1ty <venvaropt@gmail.com>
2019-08-18 00:20:07 +0300

    Update perl_installer.sh

--- Perl/Perl6/perl_installer.sh
+++ Perl/Perl6/perl_installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 wget https://rakudostar.com/files/star/rakudo-star-2019.03.tar.gz
-tar xfz rakudo-star-2019.03.tar.gz
+  tar xfz rakudo-star-2019.03.tar.gz
 cd rakudo-star-2019.03
-perl Configure.pl --gen-moar --make-install --prefix ~/rakudo
+  perl Configure.pl --gen-moar --make-install --prefix ~/rakudo
 exit 0;
