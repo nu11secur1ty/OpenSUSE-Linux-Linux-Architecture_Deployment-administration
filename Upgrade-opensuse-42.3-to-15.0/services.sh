4b42b4e864453e293013ece5e454e9e5605bae98
nu11secur1ty <venvaropt@gmail.com>
2019-08-29 16:31:27 +0300

    Create services.sh

---
+++ Upgrade-opensuse-42.3-to-15.0/services.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+  zypper addrepo http://download.opensuse.org/repositories/devel:/languages:/php:/php56/openSUSE_Leap_42.3/devel:languages:php:php56.repo
+      sleep 5;
+  zypper refresh
+  zypper up -y
+  zypper dup -y
+      sleep 3;
+  zypper in apache2 -y    
+  zypper in -y php5 php5-mysql apache2-mod_php5
+      a2enmod php5
+exit 0;
