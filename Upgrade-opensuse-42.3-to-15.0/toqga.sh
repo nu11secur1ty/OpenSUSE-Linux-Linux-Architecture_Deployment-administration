043ba6253ca037682fb1485942313e2540f0d828
nu11secur1ty <venvaropt@gmail.com>
2019-08-29 16:26:00 +0300

    Update toqga.sh

--- Upgrade-opensuse-42.3-to-15.0/toqga.sh
+++ Upgrade-opensuse-42.3-to-15.0/toqga.sh
@@ -5,9 +5,13 @@ zypper dup -y
 sed -i 's/42.3/15.0/' /etc/zypp/repos.d/*
   zypper refresh
     zypper dup --download-in-advance -y
-      sleep 3;
+      sleep 5;
     zypper addrepo http://download.opensuse.org/repositories/devel:/languages:/php:/php56/openSUSE_Leap_42.3/devel:languages:php:php56.repo
-  zypper in apache2 -y    
+      sleep 5;
+      zypper up -y
+      zypper dup -y
+          sleep 3;
+        zypper in apache2 -y    
       zypper in -y php5 php5-mysql apache2-mod_php5
       a2enmod php5
   telinit 6
