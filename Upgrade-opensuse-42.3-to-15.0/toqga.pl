c4c304c759ed454e82eec807544848297acb0fb3
nu11secur1ty <venvaropt@gmail.com>
2019-08-30 11:04:00 +0300

    Update toqga.pl

--- Upgrade-opensuse-42.3-to-15.0/toqga.pl
+++ Upgrade-opensuse-42.3-to-15.0/toqga.pl
@@ -4,7 +4,8 @@ use strict;
 use diagnostics;
 
 my $check_lat_up = `zypper up -y`;
+my $check_lat_dup = `zypper dup -y`;
 my $addnew_repo = `sed -i 's/42.3/15.0/' /etc/zypp/repos.d/*`;
 my $refresh_new_repo = `zypper refresh`;
-my $upgrade_new_repo = `zypper dup --download-in-advance`;
+my $upgrade_new_repo = `zypper dup --download-in-advance -y`;
 my $reload = `telinit 6`;
