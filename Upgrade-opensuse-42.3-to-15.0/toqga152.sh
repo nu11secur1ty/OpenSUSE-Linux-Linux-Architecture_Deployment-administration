0c14883f93dd980f5643eec350882e709eb31d4a
nu11secur1ty <venvaropt@gmail.com>
2019-10-02 14:13:17 +0300

    Create toqga152.sh

---
+++ Upgrade-opensuse-42.3-to-15.0/toqga152.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# Author @nu11secur1ty
+# Update old version
+zypper up -y
+zypper dup -y
+# Upgrade 
+  sleep 3;
+sed -i 's/15.1/15.2/' /etc/zypp/repos.d/*
+  zypper refresh
+    mv /usr/share/doc/packages/kernel-syms/README.SUSE /usr/share/doc/packages/kernel-syms/README.SUSE.BACK
+  zypper up -y
+  zypper dup -y
+  zypper dup --download-in-advance
+  telinit 6
+exit 0;
