ceb8c4b7f000ab9bde07a51f7aa0d3c605405206
nu11secur1ty <venvaropt@gmail.com>
2019-10-02 14:03:02 +0300

    Update toqga151.sh

--- Upgrade-opensuse-42.3-to-15.0/toqga151.sh
+++ Upgrade-opensuse-42.3-to-15.0/toqga151.sh
@@ -7,9 +7,9 @@ zypper dup -y
   sleep 3;
 sed -i 's/15.0/15.1/' /etc/zypp/repos.d/*
   zypper refresh
+    mv /usr/share/doc/packages/kernel-syms/README.SUSE /usr/share/doc/packages/kernel-syms/README.SUSE.BACK
   zypper up -y
   zypper dup -y
-  zypper dup --download-in-advance 
-    mv /usr/share/doc/packages/kernel-syms/README.SUSE /usr/share/doc/packages/kernel-syms/README.SUSE.BACK
+  zypper dup --download-in-advance
   telinit 6
 exit 0;
