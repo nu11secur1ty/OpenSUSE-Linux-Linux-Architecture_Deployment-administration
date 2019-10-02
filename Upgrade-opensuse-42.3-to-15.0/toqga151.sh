9ff3b28e33402488c5da4be6845388c82aa8526d
nu11secur1ty <venvaropt@gmail.com>
2019-10-02 13:34:37 +0300

    Update toqga151.sh

--- Upgrade-opensuse-42.3-to-15.0/toqga151.sh
+++ Upgrade-opensuse-42.3-to-15.0/toqga151.sh
@@ -4,7 +4,7 @@ zypper up -y
 zypper dup -y
 # Upgrade 
   sleep 3;
-sed -i 's/42.3/15.1/' /etc/zypp/repos.d/*
+sed -i 's/15.0/15.1/' /etc/zypp/repos.d/*
   zypper refresh
   zypper dup --download-in-advance 
   telinit 6
