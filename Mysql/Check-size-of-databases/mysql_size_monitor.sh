2f83859a8fb872baac3899d022a1954ee855109c
nu11secur1ty <venvaropt@gmail.com>
2019-10-17 10:29:55 +0300

    Create mysql_size_monitor.sh

---
+++ Mysql/Check-size-of-databases/mysql_size_monitor.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+# Author V.Varbanovski @nu11secur1ty
+
+truncate -s 0 /path/to/your/report/file/sqlreport.log
+
+echo "Mysql-proc-monitor by V.Varbanovski ;)";
+echo ""
+echo ""
+echo "===Time==="
+timedatectl
+echo "" 
+echo "===Zombi==="
+ps aux | awk {'print $8'}|grep -c Z
+echo ""
+echo "===Load-average==="
+cat /proc/loadavg
+echo ""
+echo ""
+echo "==============================================================="
+# Input your user
+user="user_name"
+# Input your password
+password="password"
+
+# Checking a global status
+#echo "===STATUS===";
+#       mysql -u $user -p$password -e "SHOW GLOBAL STATUS;"
+#       echo ""
+
+echo "===Size of databases===";
+mysql -u $user -p$password <<EOFMYSQL
+SELECT table_schema AS "Database", SUM(data_length + index_length) / 1024 / 1024 / 1024 AS "Size (GB)" FROM information_schema.TABLES GROUP BY table_schema\G;
+EOFMYSQL
+        echo ""
+echo "===Processlist===";
+        mysql -u $user -p$password -e "SHOW PROCESSLIST;"
+        echo ""
+echo "===Queriesi===";
+        mysql -u $user -p$password -e "SHOW FULL PROCESSLIST\G;"
+        echo ""
+echo "===WARNINGS===";
+        mysql -u $user -p$password -e "SHOW COUNT(*) WARNINGS;"
+
+exit 0;
