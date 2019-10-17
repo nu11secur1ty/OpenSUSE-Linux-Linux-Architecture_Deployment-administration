ad9da42eea5f7b51c126434d98d8c97bc167ea33
nu11secur1ty <venvaropt@gmail.com>
2019-10-17 13:29:58 +0300

    Update mysql_drupai_i_otnasai.sh

--- Mysql/Backup/mysql_drupai_i_otnasai.sh
+++ Mysql/Backup/mysql_drupai_i_otnasai.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 ######################################
-# Create Backup every night in 00:30 #
+# Create Backup every night in 00:00 #
 #         by V.Varbanovski           #
 ######################################
 
 # Dump mysql bases.
 user=your_username
 pass=your_password
-
+# All bases with content
 #mysqldump -u $user -p$pass --opt --all-databases | gzip > /your/path/to/save/all_$(date "+%Y-%m-%d").sql.gz
-
+# Without content
+# mysqldump -u $user -p$pass --no-data base1 | gzip > /your/path/to/save/base1$(date "+%Y-%m-%d").sql.gz
 mysqldump -u $user -p$pass base1 | gzip > /your/path/to/save/base1$(date "+%Y-%m-%d").sql.gz
 mysqldump -u $user -p$pass base2 | gzip > /your/path/to/save/base2$(date "+%Y-%m-%d").sql.gz
 mysqldump -u $user -p$pass base3 | gzip > /your/path/to/save/base3$(date "+%Y-%m-%d").sql.gz
