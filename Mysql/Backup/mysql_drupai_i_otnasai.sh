bbdab8ee66359966bf59b3ed387825d9f13d3a56
nu11secur1ty <venvaropt@gmail.com>
2019-12-11 11:29:05 +0200

    Update mysql_drupai_i_otnasai.sh

--- Mysql/Backup/mysql_drupai_i_otnasai.sh
+++ Mysql/Backup/mysql_drupai_i_otnasai.sh
@@ -17,4 +17,7 @@ mysqldump -u $user -p$pass base3 | gzip > /your/path/to/save/base3$(date "+%Y-%m
 mysqldump -u $user -p$pass base4 | gzip > /your/path/to/save/base4$(date "+%Y-%m-%d").sql.gz
 mysqldump -u $user -p$pass base5 | gzip > /your/path/to/save/base5$(date "+%Y-%m-%d").sql.gz
 
+# Single without data
+mysqldump -u $user -p$pass --no-data base | gzip > /your/path/to/save/base$(date "+%Y-%m-%d").sql.gz
+
 exit 0;
