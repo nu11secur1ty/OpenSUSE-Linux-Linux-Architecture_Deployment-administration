d59c502ff6aafcf234d505f85842da848f8eecc3
nu11secur1ty <venvaropt@gmail.com>
2020-04-08 09:50:30 +0300

    Update toqga152.sh

--- Upgrade-opensuse-42.3-to-15.0/toqga152.sh
+++ Upgrade-opensuse-42.3-to-15.0/toqga152.sh
@@ -6,7 +6,10 @@ zypper dup -y
 # Upgrade 
   sleep 3;
 rm /etc/zypp/repos.d/openSUSE-Leap-15.1-1.repo
-sed -i 's/15.1/15.2/' /etc/zypp/repos.d/*
+## Sources
+# $releasever
+# 15.1
+sed -i 's/$releasever/15.2/' /etc/zypp/repos.d/*
   zypper refresh
     mv /usr/share/doc/packages/kernel-syms/README.SUSE /usr/share/doc/packages/kernel-syms/README.SUSE.BACK
   zypper up -y
