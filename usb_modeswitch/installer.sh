54ce18fcfbb5d0c972e6ca6e576d3a6f0c2bb48c
nu11secur1ty <venvaropt@gmail.com>
2020-10-20 16:13:19 +0300

    Update installer.sh

--- usb_modeswitch/installer.sh
+++ usb_modeswitch/installer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 zypper addrepo https://download.opensuse.org/repositories/openSUSE:Factory/standard/openSUSE:Factory.repo
 zypper -n refresh 
-zypper install -n usb_modeswitch
+zypper -n install usb_modeswitch
